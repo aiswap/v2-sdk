50b8c8b3ec3a759a0a6584bcb4b297e4903380c8
TengYingWei <734970600@qq.com>
2021-03-19 14:29:48 +0800

    first commit

---
+++ dist/constants.d.ts
@@ -0,0 +1,9 @@
+import JSBI from 'jsbi';
+export declare const FACTORY_ADDRESS = "0x386CbD55927c1FEC057D30Fe68A6dB656e8C31dC";
+export declare const INIT_CODE_HASH = "71d15772a2b431bfcb85fd38973fe760b5765f961d5586544c62fabc8ba01d3c";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const FIVE: JSBI;
+export declare const _997: JSBI;
+export declare const _1000: JSBI;
---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+import JSBI from 'jsbi'
+
+export const FACTORY_ADDRESS = '0x386CbD55927c1FEC057D30Fe68A6dB656e8C31dC'
+
+export const INIT_CODE_HASH = '71d15772a2b431bfcb85fd38973fe760b5765f961d5586544c62fabc8ba01d3c'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const FIVE = JSBI.BigInt(5)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)
